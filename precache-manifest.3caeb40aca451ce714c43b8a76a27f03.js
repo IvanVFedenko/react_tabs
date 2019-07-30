bee00db80a3fa3655d3e84059b4ffa5a33980ff0
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-07-30 16:02:39 +0300

    Updates

---
+++ precache-manifest.3caeb40aca451ce714c43b8a76a27f03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496221bc246d03bdb846f21912310a86",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "d655dc780a5539066e11",
+    "url": "/react_tabs/static/css/main.490812b8.chunk.css"
+  },
+  {
+    "revision": "a816567f6997c7470b61",
+    "url": "/react_tabs/static/js/2.eff7d8e5.chunk.js"
+  },
+  {
+    "revision": "d655dc780a5539066e11",
+    "url": "/react_tabs/static/js/main.d28f1710.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
